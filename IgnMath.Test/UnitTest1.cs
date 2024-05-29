658918f540b1131f8bfd8a9f36812e35813c88a5
ignacio <lasterriens@gmail.com>
2024-05-28 22:23:55 -0400

    tests

--- IgnMath.Test/UnitTest1.cs
+++ IgnMath.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace IgnMath.Test;
 
 public class Tests
@@ -12,13 +14,32 @@ public class Tests
     {
         var combi = new Combinations<string>(new List<IEnumerable<string>>()
         {
-            new List<string>() { "a", "b", "c" },
-            new List<string>() { "d", "e", "f" },
-            new List<string>() { "g", "h", "i" },
+            new List<string>() { "a", "b", },
+            new List<string>() { "d", },
         });
 
-        var res = combi.GetCombinations();
+        var res = combi.GetCombinations().ToList();
 
-        foreach (var enumerable in res) Console.WriteLine(string.Join(", ", enumerable));
+        res.Should().HaveCount(2);
+        res.Should().ContainEquivalentOf(new List<string>() { "a", "d" });
+        res.Should().ContainEquivalentOf(new List<string>() { "b", "d" });
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var combi = new Combinations<string>(new List<IEnumerable<string>>()
+        {
+            new List<string>() { "a", "b", },
+            new List<string>() { "d", "e", },
+        });
+
+        var res = combi.GetCombinations().ToList();
+
+        res.Should().HaveCount(4);
+        res.Should().ContainEquivalentOf(new List<string>() { "a", "d" });
+        res.Should().ContainEquivalentOf(new List<string>() { "a", "e" });
+        res.Should().ContainEquivalentOf(new List<string>() { "b", "d" });
+        res.Should().ContainEquivalentOf(new List<string>() { "b", "e" });
     }
 }
